3f89b523c7635a0fdfb503cb396d3154b71d7154
Abyssmash <lime920506@naver.com>
2024-08-06 11:52:20 +0900

    00자료형

--- _00변수/Test01.java
+++ _00변수/Test01.java
@@ -2,4 +2,12 @@ package _00변수;
 
 public class Test01 {
 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 정수 변수 선언
+		int a =10;
+		byte aa =10;
+		System.out.println(a);
+		System.out.println(aa);
+	}
 }

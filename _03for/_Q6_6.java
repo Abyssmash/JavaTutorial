6fe9c2f8cb95e2510321f5e0140e60297db00970
aa <aa@a.com>
2024-08-12 17:37:39 +0900

    test

--- _03for/_Q6_6.java
+++ _03for/_Q6_6.java
@@ -10,9 +10,14 @@ public class _Q6_6 {
 		// 수식 계산 규칙은 연산자 우선순위 없이 앞에서 부터 차례대로 계산한다.
 		// 지금까지 공부한 내용으로 해결하시고, 형변환만 검색이 가능합니다. 
 		// 메서드는 최대한 적게 사용하세요
-		
 		String a="23-56+45*2-56";
 		// 10진수 +:75, -:77, *:74
-		
+		String str1="23";
+		String str2="56";
+		String str3="45";
+		String str4="2";
+		String str5="56";
+		int numInt = Integer.parseInt(str1);
+		System.out.println(numInt-str2+str3*str4-str5);
 	}
 }

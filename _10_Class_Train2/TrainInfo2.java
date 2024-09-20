de7c888d04bbc3d35dc78faac5c5703f8e51d531
Abyssmash <lime920506@gmail.com>
2024-09-20 17:27:56 +0900

    java

---
+++ _10_Class_Train2/TrainInfo2.java
@@ -0,0 +1,23 @@
+package _10_Class_Train2;
+
+public class TrainInfo2 {
+	
+	public TrainInfo2() {
+		
+		int[] trainNum = {1111, 2222, 3333, 4444};
+		String[] trainType = new String[] {"무궁화", "새마을", "무궁화", "새마을"};
+		
+		// 인덱스 번호로 매칭하려고 했는데 변수 타입이 달라서 for문으로 비교가 불가?
+		// 아니면 짝수 + 홀수 나누기
+	
+	}
+		public void prt() {
+							
+				
+			}
+		}			
+
+
+		
+
+
--- _10_Class_Train2/TrainMain2.java
+++ _10_Class_Train2/TrainMain2.java
@@ -1,6 +1,6 @@
 package _10_Class_Train2;
 
-public class TrainMain {
+public class TrainMain2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub

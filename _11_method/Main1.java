d746a1ac8b84887b78f7c64c2613fb00e86cf211
Abyssmash <lime920506@gmail.com>
2024-09-24 12:49:34 +0900

    java

--- _11_method/Main1.java
+++ _11_method/Main1.java
@@ -3,8 +3,16 @@ package _11_method;
 public class Main1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		TestMethod t = new TestMethod();
+		System.out.println(t.name);
+		t.setName("hong");
+		System.out.println(t.name);
+		System.out.println(t.getName());
+		String resultName = t.getName2("1122");
+		if(resultName!=null) {
+			System.out.println(resultName);
+		}else {
+			System.out.println("비번틀림");
+		}System.out.println(t.maskName("***"));
 	}
-
 }
--- _11_method/TestMethod.java
+++ _11_method/TestMethod.java
@@ -2,12 +2,30 @@ package _11_method;
 
 public class TestMethod {
 		// 생성자는 눈에 안보이지만 생략되어있음
+		
+		// 이름 뒤에 마스킹을 해주는 메소드 만들기
+		// 호출할 떄 문자열로 마스킹 문자를 지정하면, (*매개변수)
+		// 이름뒤에 마스킹 문자를 연결하여(*기능) 리턴한다.
+		// 메소드명은 maskName
 	
 		String name = null;
+		public String getName2(String pwd) {
+			if(pwd.equals("1111")) {
+				return name;
+			}else {
+				return null;
+			}
+		}
 		public void setName(String n) {
 			name = n;
 		}
 		public String getName() {
 			return name;
 		}
+			
+		public String maskName(String masking) {		// 정의
+			String maskingName = name+masking;
+			return maskingName;
+		}
+		
 }

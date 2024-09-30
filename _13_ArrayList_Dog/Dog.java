1d06ecad57f98d3a6cee49f681320ca26681d540
Abyssmash <lime920506@gmail.com>
2024-09-30 17:27:44 +0900

    java

---
+++ _13_ArrayList_Dog/Dog.java
@@ -0,0 +1,6 @@
+package _13_ArrayList_Dog;
+
+public class Dog {	
+	String dogName = null;		
+}
+
---
+++ _13_ArrayList_Dog/Main.java
@@ -0,0 +1,9 @@
+package _13_ArrayList_Dog;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new dogList();
+	}
+}

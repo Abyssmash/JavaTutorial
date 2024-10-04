75705283d45035d7fc9b5aed643740c4aaf55a0a
Abyssmash <lime920506@gmail.com>
2024-10-04 11:52:23 +0900

    java

--- School/StuMge.java
+++ School/StuMge.java
@@ -1,12 +1,17 @@
 package School;
+
+import java.util.ArrayList;
+
 // 요구사항에서 학생을 관리하는 내용을 추상화
 public class StuMge {
+	private ArrayList<Stu> slist = new ArrayList<>();
 	
 	public void menu() {
 		System.out.println("1. 등록");
-		System.out.println();
+		System.out.println("2. 검색");
+		System.out.println("3. 전체보기");
+		System.out.println("4. 삭제");
 	}
-
 	private void list() {
 		
 	}
